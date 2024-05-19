2a51d99701b75753c39fc978233bb0ae79f9dbfa
Davide Ghiotto <davide.ghiotto@bitrock.it>
2024-05-19 18:05:48 +0200

    enhancer with correct evolution stage computation

--- database/enhancer.js
+++ database/enhancer.js
@@ -4,19 +4,51 @@ import axios from "axios";
 import { writeFileSync } from "fs";
 import { join } from "path";
 
+const getPokemonFromEvolutionChain = async (evolutionChainId) => {
+  const evolutionChain = await axios.get(
+    `https://pokeapi.co/api/v2/evolution-chain/${evolutionChainId}`
+  );
+  const { data } = evolutionChain;
+  const pokemonEvolutions = [];
+  const getPokemon = (chain, stage) => {
+    const id = chain.species.url.split("/")[6];
+    if (id <= 151 && chain.species)
+      pokemonEvolutions.push({
+        id: Number(id),
+        name: chain.species.name,
+        stage,
+      });
+
+    if (chain.evolves_to) {
+      chain.evolves_to.forEach((evolution) => {
+        getPokemon(evolution, id <= 151 ? stage + 1 : stage);
+      });
+    }
+  };
+  getPokemon(data.chain, 1);
+  return pokemonEvolutions;
+};
+
+const gatherEvolutionChainInfo = async () => {
+  const promises = [];
+  for (let i = 1; i <= 78; i++) promises.push(getPokemonFromEvolutionChain(i));
+
+  const pokemonEvolutions = await Promise.all(promises);
+  return pokemonEvolutions.flat();
+};
+
 const enhancer = async () => {
   const pokeData = [];
+  const pokemonEvolutionData = await gatherEvolutionChainInfo();
   for (let i = 1; i <= 151; i++) {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
     const { data } = response;
     const speciesResponse = await axios.get(data.species.url);
     const { data: speciesData } = speciesResponse;
     const color = speciesData.color.name;
-    const evolutionChainResponse = await axios.get(
-      speciesData.evolution_chain.url
-    );
-    const { data: evolutionChainData } = evolutionChainResponse;
-    const evolutionStage = evolutionChainData.chain.evolves_to.length; // TODO: this is not correct
+    const evolutionStage = pokemonEvolutionData.find(
+      (poke) => poke.id === i
+    ).stage;
     const habitat = speciesData.habitat ? speciesData.habitat.name : "unknown";
     const enhancedData = { id: data.id, color, evolutionStage, habitat };
     pokeData.push(enhancedData);
