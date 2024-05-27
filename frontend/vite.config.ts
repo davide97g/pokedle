5fcd06b5aef84a67d2b58250bb65f0e1fdb231e7
Davide Ghiotto <davide.ghiotto@bitrock.it>
2024-05-27 23:46:12 +0200

    pwa installable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Pokèdle",
+        short_name: "Pokèdle",
+        description: "Pokèdle: guess the pokèmon",
+        theme_color: "#3B096C",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
