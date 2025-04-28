fb7fce2a6cc4cbf55afb7a7e89084773d9d08600
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-04-28 19:56:04 +0200

    feat: implement routing with About and Player pages, and restructure provider setup

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import { RouterProvider } from "react-router-dom";
-
+import { Route, Routes } from "react-router-dom";
 import { LayoutProvider } from "./context/LayoutProvider";
 import { StatusProvider } from "./context/StatusProvider";
-import { router } from "./router";
+import About from "./pages/About";
+import Player from "./pages/Player";
 
 export default function App() {
   return (
     <main className="purple-dark w-screen h-screen text-foreground bg-background text-center items-center justify-start sm:gap-10 gap-4 flex flex-col overflow-x-auto sm:overflow-x-auto pb-5">
       <LayoutProvider>
         <StatusProvider>
-          <RouterProvider router={router} />
+          <Routes>
+            <Route element={<Player />} path="/" />
+            <Route element={<About />} path="/about" />
+          </Routes>
         </StatusProvider>
       </LayoutProvider>
     </main>
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,19 @@
-import { HeroUIProvider } from "@heroui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import { Provider } from "./provider";
+
+import { BrowserRouter } from "react-router-dom";
+import "./styles/global.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HeroUIProvider>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </HeroUIProvider>
+  <BrowserRouter>
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </BrowserRouter>
 );
