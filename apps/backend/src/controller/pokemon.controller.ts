d19d9a67bcf84dd11e4ac70c7f644ea7b1622e7d
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-04-29 14:56:48 +0200

    feat: add guess controller and update routes for guess validation

--- apps/backend/src/controller/pokemon.controller.ts
+++ apps/backend/src/controller/pokemon.controller.ts
@@ -1,5 +1,4 @@
-import { NextFunction, Request, Response, type Express } from "express";
-import { computeValidationGuess } from "../services/guess.service";
+import { type Express } from "express";
 import { searchPokemon } from "../services/pokemon.service";
 
 export const createPokemonController = (app: Express) => {
@@ -19,37 +18,4 @@ export const createPokemonController = (app: Express) => {
       res.status(500).send("Internal Server Error");
     }
   });
-
-  // get guess feedback results
-  app.post(
-    "/pokemon/guess/:pokemonId",
-    async (
-      req: Request<{ pokemonId: string }>,
-      res: Response,
-      _: NextFunction
-    ) => {
-      try {
-        const { pokemonId } = req.params;
-        if (!pokemonId) {
-          res.status(400).send("Pokemon Id is required");
-        }
-        const pokemonIdNumber = Number(pokemonId);
-        if (isNaN(pokemonIdNumber)) {
-          res.status(400).send("Invalid Pokemon Id");
-        }
-
-        const validationGuess = await computeValidationGuess(pokemonIdNumber);
-
-        if (!validationGuess) {
-          res.status(400).send("Pokemon not found");
-        }
-        res.status(200).send({
-          validationGuess,
-        });
-      } catch (error) {
-        console.error("Error computing the validation of your guess", error);
-        res.status(500).send("Internal Server Error");
-      }
-    }
-  );
 };
