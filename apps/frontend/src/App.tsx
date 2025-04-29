d393050680cac7a6ecad895b8fa893ebbef9fe86
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-04-29 22:41:05 +0200

    feat: add ServerReady component and integrate it into App for system readiness check

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import ServerReady from "./components/ServerReady";
 import { LayoutProvider } from "./context/LayoutProvider";
 import { StatusProvider } from "./context/StatusProvider";
 import About from "./pages/About";
@@ -7,14 +8,16 @@ import Player from "./pages/Player";
 export default function App() {
   return (
     <main className="container purple-dark w-screen h-screen text-foreground bg-background text-center items-center justify-start sm:gap-10 gap-4 flex flex-col overflow-x-auto sm:overflow-x-auto pb-5">
-      <LayoutProvider>
-        <StatusProvider>
-          <Routes>
-            <Route element={<Player />} path="/" />
-            <Route element={<About />} path="/about" />
-          </Routes>
-        </StatusProvider>
-      </LayoutProvider>
+      <ServerReady>
+        <LayoutProvider>
+          <StatusProvider>
+            <Routes>
+              <Route element={<Player />} path="/" />
+              <Route element={<About />} path="/about" />
+            </Routes>
+          </StatusProvider>
+        </LayoutProvider>
+      </ServerReady>
     </main>
   );
 }
---
+++ apps/frontend/src/hooks/system/useSystemGetInfo.tsx
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
+export const useSystemGetInfo = () => {
+  return useQuery({
+    queryKey: ["systemInfo"],
+    queryFn: async () => {
+      const { VITE_BACKEND_URL } = import.meta.env;
+      const response = await fetch(`${VITE_BACKEND_URL}/`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.ok;
+    },
+    refetchInterval: 5000,
+  });
+};
