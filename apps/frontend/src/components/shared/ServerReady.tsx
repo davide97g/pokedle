d2d9de9e63754ca8ab1790fa22c1a8efb0e6cc36
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-05-06 21:16:06 +0200

    feat: loading server info message

--- apps/frontend/src/components/shared/ServerReady.tsx
+++ apps/frontend/src/components/shared/ServerReady.tsx
@@ -5,7 +5,18 @@ import { Loader } from "./loader/Loader";
 export default function ServerReady({ children }: { children: ReactNode }) {
   const systemInfo = useSystemGetInfo();
 
-  if (systemInfo.isFetching) return <Loader />;
+  if (systemInfo.isFetching)
+    return (
+      <>
+        <Loader />
+        <div className="m-auto pt-32">
+          <h1 className="text-xl text-primary-400">Server is not ready</h1>
+          <p className="text-primary-300">
+            Please wait a moment and refresh the page.
+          </p>
+        </div>
+      </>
+    );
   if (systemInfo.isError) {
     console.error(systemInfo.error);
     return (
