3ccca77be7bece70dda5b8ca25083f99100a7053
Davide Ghiotto <dghiotto.dev@gmail.com>
2025-05-11 23:30:41 +0200

    fix hidden pokemon dynamic gen

--- apps/backend/src/data/hidden.ts
+++ apps/backend/src/data/hidden.ts
@@ -2,7 +2,8 @@ import dayjs from "dayjs";
 import { getFirestoreDatabase } from "../config/firebase";
 import { generationLimits } from "../constants";
 
-let hiddenPokemonId = 0;
+let hiddenPokemonIds: GenerationRecord[] = [];
+
 const day = dayjs().format("YYYY-MM-DD");
 
 type GenerationRecord = {
@@ -10,7 +11,7 @@ type GenerationRecord = {
   id: number;
 };
 
-async function retrieveRemoteHiddenPokemonForDay(day: string, gen: number = 1) {
+async function retrieveRemoteHiddenPokemonForDay(day: string) {
   const db = getFirestoreDatabase();
   const docRef = db.collection("guesses").doc(day);
   const data = (await docRef.get()).data() as
@@ -21,44 +22,23 @@ async function retrieveRemoteHiddenPokemonForDay(day: string, gen: number = 1) {
       gen: index + 1,
       id: Math.floor(Math.random() * limit) + 1,
     }));
-
     await db.collection("guesses").doc(day).set({ randomIdList });
-    return randomIdList.find((item) => item.gen === gen)?.id;
+    return randomIdList;
   }
-  const dailyHiddenPokemonId = data.randomIdList.find(
-    (item) => item.gen === gen
-  )?.id;
-  if (!dailyHiddenPokemonId) {
-    throw new Error("Hidden Pokemon ID not found");
-  }
-  if (typeof dailyHiddenPokemonId !== "number") {
-    throw new Error("Hidden Pokemon ID is not a number");
-  }
-  if (dailyHiddenPokemonId < 1) {
-    throw new Error("Hidden Pokemon ID is less than 1");
-  }
-  if (dailyHiddenPokemonId > 1025) {
-    throw new Error("Hidden Pokemon ID is greater than 1025");
-  }
-  if (dailyHiddenPokemonId % 1 !== 0) {
-    throw new Error("Hidden Pokemon ID is not an integer");
-  }
-  if (dailyHiddenPokemonId === 0) {
-    throw new Error("Hidden Pokemon ID is 0");
-  }
-  return dailyHiddenPokemonId;
+  return data.randomIdList;
 }
 
 export async function getPokemonIdToGuess(gen: number) {
-  if (!hiddenPokemonId) {
-    const newHiddenPokemonId = await retrieveRemoteHiddenPokemonForDay(
-      day,
-      gen
-    );
-    if (!newHiddenPokemonId) {
-      throw new Error("Hidden Pokemon not found");
-    }
-    hiddenPokemonId = newHiddenPokemonId;
-  }
-  return hiddenPokemonId;
+  if (!hiddenPokemonIds.length)
+    hiddenPokemonIds = await retrieveRemoteHiddenPokemonForDay(day);
+
+  if (!hiddenPokemonIds.length) throw new Error("Hidden Pokemons not found");
+
+  const dailyHiddenPokemonId = hiddenPokemonIds.find(
+    (item) => item.gen === gen
+  )?.id;
+  if (!dailyHiddenPokemonId)
+    throw new Error("Daily hidden Pokemon ID not found");
+
+  return dailyHiddenPokemonId;
 }
